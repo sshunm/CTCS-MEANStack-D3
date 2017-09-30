7edb59bab2859b48aad13d2c5f26bab31804e5dc
Shunmuga Prabhu Siddharthan <Shunmuga Prabhu Siddharthan>
2017-09-30 21:22:10 +0200

    added components and linked with nodejs

--- Index.js
+++ Index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const port = 3000;
+const path = require('path');
 
+app.use(express.static(__dirname + '/Client/dist/'));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello prabhu')
+  res.sendfile(path.join(__dirname + '/Client/dist/index.html'));
 })
 
 
---
+++ webconfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+    uri: 'mongodb://localhost:271017/',
+    secret: '',
+    db: 'mean-angular2'
+}
