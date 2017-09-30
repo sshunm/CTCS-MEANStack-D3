88e9ce349c193c2af9a2d8c14e04372c7d4a75d2
Shunmuga Prabhu Siddharthan <Shunmuga Prabhu Siddharthan>
2017-10-01 01:12:52 +0200

    home page prototype

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CarouselComponent } from './components/carousel/carousel.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
