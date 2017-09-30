d34682c49a0781d102ae68e2c0080c269ed4a9d7
Shunmuga Prabhu Siddharthan <Shunmuga Prabhu Siddharthan>
2017-09-30 18:47:07 +0200

    angular setup

--- Index.js
+++ Index.js
@@ -1,11 +1,17 @@
-var express = require('express')
-var app = express()
-var port = 3000;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const port = 3000;
+
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello prabhu')
 })
 
+
 app.listen(port, function() {
   console.log("Server listening in Port:" + port);
 })
+
